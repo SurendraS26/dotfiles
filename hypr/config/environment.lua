26a0dd99bd266b2fe54a6ef11da25356570c5b55
SurendraS26 <surendrasonusep26@gmail.com>
2026-06-17 23:43:39 +0530

    New hyprtoolkitconfig

--- hypr/config/environment.lua
+++ hypr/config/environment.lua
@@ -1,2 +1,26 @@
+--------------------------
+--- XDG Specifications ---
+--------------------------
+hl.env("XDG_CURRENT_DESKTOP", "Hyprland")
+hl.env("XDG_SESSION_TYPE", "wayland")
+hl.env("XDG_SESSION_DESKTOP", "Hyprland")
+
+
+-----------------------------
+--- Cursor customizations ---
+-----------------------------
+hl.env("XCURSOR_THEME","Bibata-Original-Classic")
 hl.env("XCURSOR_SIZE", "24")
 hl.env("HYPRCURSOR_SIZE", "24")
+
+
+-----------------
+--- GTK Theme ---
+-----------------
+hl.env("GTK_THEME","Flat-Remix-GTK-Magenta-Darkest")
+
+-----------------------
+--- Nvidia Specific ---
+-----------------------
+hl.env("LIBVA_DRIVER_NAME", "nvidia")
+hl.env("__GLX_VENDOR_LIBRARY_NAME", "nvidia")
--- hypr/config/monitor.lua
+++ hypr/config/monitor.lua
@@ -1,13 +1,13 @@
 hl.monitor({
     output   = "eDP-1",
-    mode     = "1920x1080@144",
+    mode     = "preffered",
     position = "auto",
     scale    = "1",
 })
 
-hl.monitor({
-    output   = "HDMI-A-5",
-    mode     = "1600x900",
-    position = "auto-left",
-    scale    = "1",
-})
+--hl.monitor({
+--    output   = "HDMI-A-5",
+--    mode     = "1600x900",
+--    position = "auto-left",
+--    scale    = "1",
+--})

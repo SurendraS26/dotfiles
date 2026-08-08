9810ea4530f0c70fc7f79f5859c25549823a670b
SurendraS26 <surendrasonusep26@gmail.com>
2026-08-08 16:30:14 +0530

    Added Responsiveness.

--- hypr/config/autostart.lua
+++ hypr/config/autostart.lua
@@ -5,6 +5,7 @@ hl.on("hyprland.start", function ()
 hl.exec_cmd("udiskie &")
 hl.exec_cmd("systemctl --user start hyprpolkitagent")
 hl.exec_cmd("wayvnc 0.0.0.0 5901")
+hl.exec_cmd("waypaper --restore")
 -- hl.exec_cmd("/home/surendra/Documents/Apps/Lenovo/legion-kb-rgb set --effect Static -c 216,27,96,216,27,96,216,27,96,216,27,96")
 end)
 

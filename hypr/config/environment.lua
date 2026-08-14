265705fbb7204ae0583a98c139951244ab5850d8
SurendraS26 <surendrasonusep26@gmail.com>
2026-08-14 21:28:18 +0530

    Switch back to Cyan

--- hypr/config/environment.lua
+++ hypr/config/environment.lua
@@ -17,7 +17,7 @@ hl.env("HYPRCURSOR_SIZE", "24")
 -----------------
 --- GTK Theme ---
 -----------------
-hl.env("GTK_THEME","Flat-Remix-GTK-Magenta-Darkest-Solid")
+hl.env("GTK_THEME","Flat-Remix-GTK-Cyan-Darkest")
 
 
 -----------------------

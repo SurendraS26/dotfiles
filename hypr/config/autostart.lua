242c41a6e66d68695f79ff1d6ce9cab1791657dc
SurendraS26 <surendrasonusep26@gmail.com>
2026-08-28 11:46:11 +0530

    Backup.

--- hypr/config/autostart.lua
+++ hypr/config/autostart.lua
@@ -4,8 +4,8 @@
 hl.on("hyprland.start", function ()
 hl.exec_cmd("udiskie &")
 hl.exec_cmd("systemctl --user start hyprpolkitagent")
-hl.exec_cmd("wayvnc --gpu --max-fps=60 0.0.0.0 5901")
+--hl.exec_cmd("wayvnc --gpu --max-fps=60 0.0.0.0 5901")
 --hl.exec_cmd("/home/surendra/Documents/apps/lenovo/legion-kb-rgb set --effect Static -c 216,27,96,216,27,96,216,27,96,216,27,96")
-hl.exec_cmd("waypaper --restore")
+hl.exec_cmd("awww-daemon")
 end)
 

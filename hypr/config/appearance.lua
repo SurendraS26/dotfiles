4696c86032290311b1a3e142304be1efb26d18cb
SurendraS26 <surendrasonusep26@gmail.com>
2026-09-09 10:20:09 +0530

    feeling bored with curves so removed the border radius curve in whole dotfiles

--- hypr/config/appearance.lua
+++ hypr/config/appearance.lua
@@ -36,7 +36,7 @@ hl.config({
 
     decoration = {
         rounding       = 10,
-        rounding_power = 2,
+        rounding_power = 0,
 
         -- Change transparency of focused and unfocused windows
         active_opacity   = 1.0,

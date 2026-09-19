296d2142b4d0ab7f208020ac9bff18a3bd7c88aa
SurendraS26 <surendrasonusep26@gmail.com>
2026-09-19 12:12:28 +0530

    Terminal 256 color support , added to environment variable

--- hypr/config/environment.lua
+++ hypr/config/environment.lua
@@ -48,3 +48,8 @@ hl.env("QT_QPA_PLATFORMTHEME","qt6ct")
 hl.env("QT_WAYLAND_DISABLE_WINDOWDECORATION", "1")
 hl.env("QT_QPA_PLATFORM", "wayland;xcb")
 hl.env("QT_AUTO_SCREEN_SCALE_FACTOR", "1")
+
+------------------------------
+--- Terminal Color Support ---
+------------------------------
+hl.env("TERM","xterm-256color")

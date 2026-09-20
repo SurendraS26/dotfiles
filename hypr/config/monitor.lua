209ab619223ddc3be78e4c165a83c926c9ce73ae
SurendraS26 <surendrasonusep26@gmail.com>
2026-09-20 09:29:23 +0530

    Add instructions to hypr/config/monitor.lua

--- hypr/config/monitor.lua
+++ hypr/config/monitor.lua
@@ -1,9 +1,19 @@
+-------------------------
+--- My laptop display ---
+-------------------------
+
 hl.monitor({
     output   = "eDP-1",
     mode     = "1920x1080@144",
     position = "auto",
     scale    = "1.25",
 })
+
+--------------------------------
+--- External monitor/display ---
+--------------------------------
+
+-- This is an example for external monitor
 
 --hl.monitor({
 --    output   = "HDMI-A-5",

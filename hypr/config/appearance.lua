643aaf984e691b0d159c0871d5dfb39324f20241
SurendraS26 <surendrasonusep26@gmail.com>
2026-09-20 09:41:46 +0530

    Add headings to hypr/config/appearance.lua

--- hypr/config/appearance.lua
+++ hypr/config/appearance.lua
@@ -1,3 +1,7 @@
+-----------------------
+--- Color Variables ---
+-----------------------
+
 local black   = "rgb(47, 52, 63)"
 local blue    = "rgb(39, 119, 255)"
 local brown   = "rgb(193, 125, 62)"
@@ -11,6 +15,10 @@ local teal    = "rgb(0, 150, 136)"
 local violet  = "rgb(124, 77, 255)"
 local white   = "rgb(250, 250, 250)"
 local yellow  = "rgb(255, 214, 0)"
+
+-----------------------
+--- Simple Settings ---
+-----------------------
 
 hl.config({
     general = {
@@ -65,13 +73,22 @@ hl.config({
     },
 })
 
+
+-------------------------
+--- Tiling Animations ---
+-------------------------
+
 hl.curve("easeOutQuint",   { type = "bezier", points = { {0.23, 1},    {0.32, 1}    } })
 hl.curve("easeInOutCubic", { type = "bezier", points = { {0.65, 0.05}, {0.36, 1}    } })
 hl.curve("linear",         { type = "bezier", points = { {0, 0},       {1, 1}       } })
 hl.curve("almostLinear",   { type = "bezier", points = { {0.5, 0.5},   {0.75, 1}    } })
 hl.curve("quick",          { type = "bezier", points = { {0.15, 0},    {0.1, 1}     } })
-
 hl.curve("easy",           { type = "spring", mass = 1, stiffness = 71.2633, dampening = 15.8273644 })
+
+
+-------------------------
+--- Window Animations ---
+-------------------------
 
 hl.animation({ leaf = "global",        enabled = true,  speed = 2,   bezier = "default" })
 hl.animation({ leaf = "border",        enabled = true,  speed = 5, bezier = "easeOutQuint" })
@@ -90,6 +107,11 @@ hl.animation({ leaf = "workspaces",    enabled = true,  speed = 2, bezier = "qui
 hl.animation({ leaf = "workspacesIn",  enabled = true,  speed = 2, bezier = "quick", style = "slidehoriz" })
 hl.animation({ leaf = "workspacesOut", enabled = true,  speed = 2, bezier = "quick", style = "slidehoriz" })
 hl.animation({ leaf = "zoomFactor",    enabled = true,  speed = 2,    bezier = "quick" })
+
+
+-----------------------------
+--- Window Manager Tweaks ---
+-----------------------------
 
 hl.config({
     dwindle = {

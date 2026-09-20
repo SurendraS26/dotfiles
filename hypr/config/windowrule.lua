13fa9b90fef4b0de20d7f7bf7f41aaa918c0e293
SurendraS26 <surendrasonusep26@gmail.com>
2026-09-20 09:38:31 +0530

    Add headings to hypr/config/windowrule.lua

--- hypr/config/windowrule.lua
+++ hypr/config/windowrule.lua
@@ -1,14 +1,15 @@
+--------------------
+--- Window Rules ---
+--------------------
+
 local suppressMaximizeRule = hl.window_rule({
-    -- Ignore maximize requests from all apps. You'll probably like this.
     name  = "suppress-maximize-events",
     match = { class = ".*" },
 
     suppress_event = "maximize",
 })
--- suppressMaximizeRule:set_enabled(false)
 
 hl.window_rule({
-    -- Fix some dragging issues with XWayland
     name  = "fix-xwayland-drags",
     match = {
         class      = "^$",
@@ -22,15 +23,6 @@ hl.window_rule({
     no_focus = true,
 })
 
--- Layer rules also return a handle.
--- local overlayLayerRule = hl.layer_rule({
---     name  = "no-anim-overlay",
---     match = { namespace = "^my-overlay$" },
---     no_anim = true,
--- })
--- overlayLayerRule:set_enabled(false)
-
--- Hyprland-run windowrule
 hl.window_rule({
     name  = "move-hyprland-run",
     match = { class = "hyprland-run" },
